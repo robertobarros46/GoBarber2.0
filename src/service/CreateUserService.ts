d461f839511738c08c710a09311b360b627e3081
Roberto de Barros Netto <roberto.netto@symphony.com>
2020-08-24 18:35:55 -0300

    Added exception handler, avatar upload and serving static files

--- src/service/CreateUserService.ts
+++ src/service/CreateUserService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -16,7 +18,7 @@ class CreateUserService {
     const checkUserExists = await userRepository.findOne({ email: email });
 
     if (checkUserExists) {
-      throw new Error('Email address already used');
+      throw new AppError('Email address already used');
     }
 
     const hasedPassword = await hash(password, 8);
